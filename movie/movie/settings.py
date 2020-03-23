6d124a249e449bf464aea5211b4674fe40dc28d2
Alibi <a.uderbay@kazdream.kz>
2020-03-24 00:54:59 +0600

    creating and displaying movies on the site

--- movie/movie/settings.py
+++ movie/movie/settings.py
@@ -51,11 +51,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'movie.urls'
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- movie/movie_app/models.py
+++ movie/movie_app/models.py
@@ -106,7 +106,7 @@ class Rating(models.Model):
     """Ratings"""
     ip = models.CharField("IP address", max_length=15)
     star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name="star")
-    movie = models.ForeignKey(Movie, on_delete=models.CharField, verbose_name="movie")
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, verbose_name="movie")
 
     def __str__(self):
         return f"{self.star} - {self.movie}"
