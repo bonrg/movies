216b7351abc8906e9ad3f2d6bc4b46fa4f7a0cfa
Alibi <a.uderbay@kazdream.kz>
2020-03-24 16:05:40 +0600

    ListView and DetailView classes, movie page

--- movie/movie_app/models.py
+++ movie/movie_app/models.py
@@ -3,6 +3,7 @@ from datetime import date
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -69,6 +70,9 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("movie_detail", kwargs={"slug": self.url})
 
     class Meta:
         verbose_name = "Movie"
--- movie/movie_app/views.py
+++ movie/movie_app/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 from .models import Movie
 
 
-class MoviesView(View):
+class MoviesView(ListView):
     """List of movies"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    # template_name = "movie_app/movies.html"
 
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movies.html", {"movie_list": movies})
 
-
+class MovieDetailView(DetailView):
+    """Full desc movie"""
+    model = Movie
+    slug_field = "url"
 
